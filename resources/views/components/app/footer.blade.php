c767427aa8c1fa6d6faa5137d2885f27c63b59d0
eidolon1302 <gigantician13@gmail.com>
2024-06-01 18:20:31 +0700

    menghapus gambar tak penting

--- resources/views/components/app/footer.blade.php
+++ resources/views/components/app/footer.blade.php
@@ -1,59 +1,11 @@
 <footer class="px-4 pb-4 bg-black z-30">
     <div class="mx-auto max-w-screen-xl">
-        <hr class="mb-6 sm:mx-auto border-slate-700 lg:mb-8" />
-        <div class="md:flex md:justify-between ">
-            <ul class="mb-4 lg:mb-0 md:mb-0">
-                <a href="{{ 'home' }}">
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Any Question?</span>
-                </a>
-                <p>manusia biasa yang hobinya tidur, lagi kuliah di ITS, dan Sedang berusaha menjadi seorang fullstack💀 </p>
-            </ul>
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-                <div>
-                    <h2 class="mb-2 text-sm font-semibold text-emerald-900 uppercase dark:text-slate-100">Social</h2>
-                    <ul class="text-gray-600 dark:text-gray-400">
-                        <li class="mb-2">
-                            <a href="#" class="hover:underline">Linkedin</a>
-                        </li>
-                        <li class="mb-2">
-                            <a href="#" class="hover:underline">Instagram</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-1 text-sm font-semibold text-emerald-900 uppercase dark:text-slate-100">Support</h2>
-                    <ul class="text-gray-600 dark:text-gray-400">
-                        <li class="mb-3">
-                            <a href="#" class="hover:underline ">Help</a>
-                        </li>
-                        <li class="mb-1">
-                            <h2 class="text-sm font-semibold text-emerald-900 uppercase dark:text-slate-100">Service</h2>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">System Status</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('roadmap') }}" class="hover:underline">Roadmap</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-1 text-sm font-semibold text-emerald-900 uppercase dark:text-slate-100">Legal</h2>
-                    <ul class="text-gray-600 dark:text-gray-400">
-                        <li class="mb-1">
-                            <a href="#" class="hover:underline">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        <div class="w-full text-center">
+            Seorang Mahasiswa yang hobinya tidur, sedang berusaha untuk menjadi seorang Ultraman🤖.
         </div>
-        <hr class="my-6 sm:mx-auto border-slate-700 lg:my-8" />
         <div class="w-full text-center">
             © 2024 Copyright:
-            <a class="text-whitehite" href="https://tw-elements.com/">Rico Fitranda</a>
+            <a class="text-whitehite" href="">Rico Fitranda</a>
         </div>
     </div>
 </footer>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -5,7 +5,7 @@
                 <div
                     class="lg:row-span-2 ring-1 dark:ring-white/10 ring-primary/5 bg-black shadow-xl dark:shadow-thick rounded-3xl p-8">
                     <div class="relative flex items-center gap-x-4">
-                        <img src="" alt=""
+                        <img src="/images/foto-saya.jpeg" alt=""
                             class="h-10 w-10 rounded-full ring-1 dark:ring-white/10 ring-primary/5" />
                         <div class="text-sm leading-6">
                             <p class="font-semibold text-white">
@@ -15,7 +15,7 @@
                                 </a>
                             </p>
                             <p class="text-zinc-500 dark:text-zinc-400">
-                                Web developer &amp; UI/UX
+                                Web Developer & UI/UX
                             </p>
                         </div>
                     </div>
@@ -42,9 +42,9 @@
                     <div
                         class="grid mt-4 grid-cols-4 md:grid-cols-8 lg:grid-cols-4 gap-y-4 gap-4 justify-center w-full">
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
-                                class="icon icon-tabler icon-tabler-mail h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50"
+                                class="icon icon-tabler icon-tabler-mail h-6 w-6 group-hover:scale-125 duration-300 text-primary text-white group-hover text-primary:text-white/50"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
                                 stroke-linecap="round" stroke-linejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
@@ -55,7 +55,7 @@
                             </svg>
                         </a>
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl shadow-thick hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler icon-tabler-brand-github h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -67,7 +67,7 @@
                             </svg>
                         </a>
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler icon-tabler-brand-behance h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -80,7 +80,7 @@
                             </svg>
                         </a>
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler icon-tabler-brand-threads h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -92,7 +92,7 @@
                             </svg>
                         </a>
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50 icon-tabler-brand-dribbble"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -105,7 +105,7 @@
                             </svg>
                         </a>
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50 icon-tabler-brand-x"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -116,7 +116,7 @@
                             </svg>
                         </a>
                         <a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50 icon-tabler-brand-linkedin"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -131,7 +131,7 @@
                                 <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
                             </svg>
                         </a><a href="#_"
-                            class="flex items-center justify-center aspect-square shadow-xl dark:shadow-thick hover:bg-zinc-100 dark:hover:bg-primary ring-1 bg-zinc-50 dark:ring-white/10 ring-primary/5 dark:bg-tertiary rounded-lg hover:ring-primary/5 dark:hover:ring-white/20 duration-300 group transition-all h-12 w-12">
+                            class="flex items-center justify-center aspect-square shadow-thick hover:bg-primary ring-1  dark:ring-white/10 ring-primary/5 bg-tertiary rounded-lg hover:ring-white/20 duration-300 group transition-all h-12 w-12">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler h-6 w-6 group-hover:scale-125 duration-300 text-primary dark:text-white group-hover text-primary:dark:text-white/50 icon-tabler-brand-facebook"
                                 viewBox="0 0 24 24" stroke-width="0.8" stroke="currentColor" fill="none"
@@ -181,7 +181,7 @@
                     </div>
                     <div class="w-full mt-8 md:max-w-xs lg:max-w-none">
                         <button type="button"
-                            class="text-sm py-2 w-full px-4 h-12 font-semibold focus:ring-2 rounded-lg bg-primary dark:bg-white dark:text-primary dark:hover:text-white dark:hover:bg-white/5 hover:bg-primary/10 text-white flex duration-200 focus:ring-offset-2 focus:ring-inline-flex items-center justify-between">
+                            class="text-sm py-2 w-full px-4 h-12 font-semibold focus:ring-2 rounded-lg bg-primary   text-white flex duration-200 focus:ring-offset-2 focus:ring-inline-flex items-center justify-between ring-1  dark:ring-white/10 ring-primary/5 hover:ring-white/20 duration-300">
                             Download my CV
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 class="icon icon-tabler icon-tabler-download w-4 h-4" viewBox="0 0 24 24"
@@ -199,34 +199,11 @@
                 <!-- Figma big logo-->
                 <a href="https://www.figma.com/@mikeandreuzza"
                     class="ring-1 lg:row-start-3 items-center h-full flex p-8 flex-col justify-center hover:ring-primary/5 dark:hover:ring-white/20 dark:ring-white/10 ring-primary/5 relative rounded-3xl overflow-hidden bg-secondary shadow-xl dark:shadow-thick">
-                    <svg class="w-24 h-24 lg:w-48 xl:h-64 xl:w-64 lg:h-48" viewBox="0 0 90 90" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0_1_136)">
-                            <path
-                                d="M45 45.5C45 42.9804 46.0009 40.5641 47.7825 38.7825C49.5641 37.0009 51.9804 36 54.5 36C57.0196 36 59.4359 37.0009 61.2175 38.7825C62.9991 40.5641 64 42.9804 64 45.5C64 48.0196 62.9991 50.4359 61.2175 52.2175C59.4359 53.9991 57.0196 55 54.5 55C51.9804 55 49.5641 53.9991 47.7825 52.2175C46.0009 50.4359 45 48.0196 45 45.5Z"
-                                fill="#1ABCFE"></path>
-                            <path
-                                d="M26 64.5C26 61.9804 27.0009 59.5641 28.7825 57.7825C30.5641 56.0009 32.9804 55 35.5 55H45V64.5C45 67.0196 43.9991 69.4359 42.2175 71.2175C40.4359 72.9991 38.0196 74 35.5 74C32.9804 74 30.5641 72.9991 28.7825 71.2175C27.0009 69.4359 26 67.0196 26 64.5V64.5Z"
-                                fill="#0ACF83"></path>
-                            <path
-                                d="M45 17V36H54.5C57.0196 36 59.4359 34.9991 61.2175 33.2175C62.9991 31.4359 64 29.0196 64 26.5C64 23.9804 62.9991 21.5641 61.2175 19.7825C59.4359 18.0009 57.0196 17 54.5 17H45Z"
-                                fill="#FF7262"></path>
-                            <path
-                                d="M26 26.5C26 29.0196 27.0009 31.4359 28.7825 33.2175C30.5641 34.9991 32.9804 36 35.5 36H45V17H35.5C32.9804 17 30.5641 18.0009 28.7825 19.7825C27.0009 21.5641 26 23.9804 26 26.5V26.5Z"
-                                fill="#F24E1E"></path>
-                            <path
-                                d="M26 45.5C26 48.0196 27.0009 50.4359 28.7825 52.2175C30.5641 53.9991 32.9804 55 35.5 55H45V36H35.5C32.9804 36 30.5641 37.0009 28.7825 38.7825C27.0009 40.5641 26 42.9804 26 45.5V45.5Z"
-                                fill="#A259FF"></path>
-                        </g>
-                        <defs>
-                            <clipPath id="clip0_1_136">
-                                <rect width="38" height="57" fill="white" transform="translate(26 17)">
-                                </rect>
-                            </clipPath>
-                        </defs>
-                    </svg>
-                    <p class="text-xl text-white lg:text-2xl mt-6">
-                        Figma Community
+                    <img src="/images/logo-saturasi.png"
+                            class="rounded-2xl duration-300 invert-0 aspect-[4/4] bg-primary/5 dark:bg-primary object-cover"
+                            alt="" />
+                    <p class="text-sm text-zinc-400 font-light mt-6">
+                        Saturasi is an online platform focused on sustainability and environmental issues, integrating an art gallery, informative blog, activist space, and circular economy initiatives. Our primary goal is to raise awareness, educate, and empower the community on environmental and sustainability matters. 
                     </p>
                 </a>
                 <a href="https://lexingtonthemes.com/"
@@ -253,9 +230,7 @@
                             Saturasi
                         </p>
                         <p class="mt-4 text-sm text-zinc-400 font-light">
-                            Free and premium multipage themes and UI Kits for freelancers,
-                            developers, businesses, and personal use. Beautifully crafted with
-                            Astro.js, and Tailwind CSS — Simple & easy to customise.
+                            aims to be a central hub for information and community for individuals and organizations passionate about the environment. By combining art, education, activism, and circular economy, we hope to promote positive and sustainable change worldwide.
                         </p>
                     </div>
                 </a>
@@ -327,7 +302,7 @@
     <section>
         <div class="mb-4 mx-4 ring-1 ring-primary/5 bg-secondary shadow-xl shadow-thick rounded-3xl p-8">
             <div class=" text-center">
-                <h1 class="mb-4 text-5xl font-extrabold leading-tight text-slate-100">START A PROJECT
+                <h1 class="mb-4 text-5xl font-extrabold leading-tight text-slate-100">SEE THE OTHER SIDE OF US
                 </h1>
                 <!-- modal join us-->
                 <div class="m-1.5">
@@ -371,23 +346,22 @@
                                         class="container px-5 py-24 mx-auto grid lg:grid-cols-2 gap-2 sm:grid-cols-1 md:grid-cols-2">
                                         <h1
                                             class="h-full p-4 justify-center text-4xl font-bold lg:col-span2 md:col-span-2 items-center text-white">
-                                            What can we help you with?</h1>
+                                            THE OTHER SIDE OF US</h1>
                                         <button
                                             class="h-full p-4 border-b border-gray-100 hover:bg-black hover:text-white lg:border-0 px-4 pt-8 pb-8 rounded-[13px] bg-white text-black">
-                                            <a class="mb-1">WEBSITE</a>
+                                            <a class="mb-1">SATURASI X CE MOVEMENT</a>
                                         </button>
                                         <button
                                             class="h-full p-4 border-b border-gray-100 hover:bg-black hover:text-white lg:border-0 px-4 pt-8 pb-8 rounded-[13px] bg-white text-black">
-                                            <a class="text-xs mb-1">WEBSITE</a>
+                                            <a class="mb-1">BHUMIASHA</a>
                                         </button>
                                         <button
                                             class="h-full p-4 border-b border-gray-100 hover:bg-black hover:text-white lg:border-0 px-4 pt-8 pb-8 rounded-[13px] bg-white text-black">
-                                            <a class="text-xs mb-1">WEBSITE</a>
+                                            <a class="mb-1">SATURASI GALLERY</a>
                                         </button>
                                         <button
                                             class="h-full p-4 border-b border-gray-100 hover:bg-black hover:text-white lg:border-0 px-4 pt-8 pb-8 rounded-[13px] bg-white text-black">
-
-                                            <a class="text-xs mb-1">WEBSITE</a>
+                                            <a class="mb-1">SATURASI ENGINEERING</a>
 
                                         </button>
                                     </div>
@@ -402,6 +376,3 @@
     </section>
 
 </div>
-{{-- <script>
-    gsap
-</script> --}}
